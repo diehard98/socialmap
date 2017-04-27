842e364938c1d606c6716b8590ad35cd3f44db32
diehard98 <diehard98@gmail.com>
2017-04-26 23:34:25 -0400

    Google map swap testing
    Swapping works

--- google_map_related.js
+++ google_map_related.js
@@ -1,6 +1,23 @@
-document.getElementById("LexingtonKY").addEventListener("click", googleMapChange);
+document.getElementById("LexingtonKY").addEventListener("click", function() {
+  googleMapChange('LEXKY');
+}, false);
 
-function googleMapChange()
+document.getElementById("LouisvilleKY").addEventListener("click", function() {
+  googleMapChange('LOUKY');
+}, false);
+
+function googleMapChange(inputParam)
 {
-  console.log("TEST");
+  if(inputParam == 'LEXKY')
+  {
+    console.log("LEXINGTON KY");
+    document.getElementById("KY_Lexington").style.display = 'block';
+    document.getElementById("KY_Louisville").style.display = 'none';
+  }
+  else if(inputParam == 'LOUKY')
+  {
+    console.log("LOUISVILLE KY");
+    document.getElementById("KY_Lexington").style.display = 'none';
+    document.getElementById("KY_Louisville").style.display = 'block';
+  }
 }
